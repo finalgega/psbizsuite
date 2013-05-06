9f3dd712505b056255a5d2082d1f86ae50cc7b85
at.lin.memyself@gmail.com <at.lin.memyself@gmail.com@75aa51da-5670-e057-c30f-8fd1125bbfc6>
2013-05-06 15:42:43 +0000

    Edited MasterPage Add ManageEmployeeDetail.aspx
    **In Controller,  public ActionResult methodName //This method is for redirecting purpose
    methodName must be the same as .aspx in Views
    Otherwise, it will give you an error**

---
+++ psbizsuite/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace psbizsuite.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult ManageEmployeeDetail()
+        {
+            return View();
+        }
+
+    }
+}
