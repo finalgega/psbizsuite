438d42692a926f33af504d440c4686e95b74e7ea
meatysalmon@gmail.com <meatysalmon@gmail.com@75aa51da-5670-e057-c30f-8fd1125bbfc6>
2013-05-13 18:48:55 +0000

    Added 4 Accounting Views, Accounting master page and accounting controller

--- trunk/psbizsuite/App_Start/RouteConfig.cs
+++ trunk/psbizsuite/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace psbizsuite
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Accounting", action = "GenerateIR", id = UrlParameter.Optional }
             );
         }
     }
