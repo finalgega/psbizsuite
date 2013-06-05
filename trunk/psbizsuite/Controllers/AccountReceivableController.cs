6c6b5ca529d9e54aa2aa58dbc68670e2067a0375
meatysalmon@gmail.com <meatysalmon@gmail.com@75aa51da-5670-e057-c30f-8fd1125bbfc6>
2013-06-05 17:57:05 +0000

    Thanks Lin!! Added CRUD for AccountPayable/Receivable for now. Will replace AccountPayable/Receivable with more detailed invoices' database values and CRUD.

---
+++ trunk/psbizsuite/Controllers/AccountReceivableController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using psbizsuite.Models;
+
+namespace psbizsuite.Controllers
+{
+    public class AccountReceivableController : Controller
+    {
+        private BizSuiteDBEntities db = new BizSuiteDBEntities();
+
+        //
+        // GET: /AccountReceivable/
+
+        public ActionResult Index()
+        {
+            var accountreceivables = db.AccountReceivables.Include(a => a.Order);
+            return View(accountreceivables.ToList());
+        }
+
+        //
+        // GET: /AccountReceivable/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            AccountReceivable accountreceivable = db.AccountReceivables.Find(id);
+            if (accountreceivable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountreceivable);
+        }
+
+        //
+        // GET: /AccountReceivable/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.Order_OrderId = new SelectList(db.Orders, "OrderId", "Status");
+            return View();
+        }
+
+        //
+        // POST: /AccountReceivable/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AccountReceivable accountreceivable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AccountReceivables.Add(accountreceivable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Order_OrderId = new SelectList(db.Orders, "OrderId", "Status", accountreceivable.Order_OrderId);
+            return View(accountreceivable);
+        }
+
+        //
+        // GET: /AccountReceivable/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            AccountReceivable accountreceivable = db.AccountReceivables.Find(id);
+            if (accountreceivable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Order_OrderId = new SelectList(db.Orders, "OrderId", "Status", accountreceivable.Order_OrderId);
+            return View(accountreceivable);
+        }
+
+        //
+        // POST: /AccountReceivable/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AccountReceivable accountreceivable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accountreceivable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Order_OrderId = new SelectList(db.Orders, "OrderId", "Status", accountreceivable.Order_OrderId);
+            return View(accountreceivable);
+        }
+
+        //
+        // GET: /AccountReceivable/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            AccountReceivable accountreceivable = db.AccountReceivables.Find(id);
+            if (accountreceivable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountreceivable);
+        }
+
+        //
+        // POST: /AccountReceivable/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AccountReceivable accountreceivable = db.AccountReceivables.Find(id);
+            db.AccountReceivables.Remove(accountreceivable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ trunk/psbizsuite/Views/AccountReceivable/Delete.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.master" Inherits="System.Web.Mvc.ViewPage<psbizsuite.Models.AccountReceivable>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Delete
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2>Delete</h2>
+
+<h3>Are you sure you want to delete this?</h3>
+<fieldset>
+    <legend>AccountReceivable</legend>
+
+    <div class="display-label">
+        <%: Html.DisplayNameFor(model => model.Status) %>
+    </div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.Status) %>
+    </div>
+
+    <div class="display-label">
+        <%: Html.DisplayNameFor(model => model.TotalAmount) %>
+    </div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.TotalAmount) %>
+    </div>
+
+    <div class="display-label">
+        <%: Html.DisplayNameFor(model => model.Order.Status) %>
+    </div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.Order.Status) %>
+    </div>
+</fieldset>
+<% using (Html.BeginForm()) { %>
+    <%: Html.AntiForgeryToken() %>
+    <p>
+        <input type="submit" value="Delete" /> |
+        <%: Html.ActionLink("Back to List", "Index") %>
+    </p>
+<% } %>
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="NavContent" runat="server">
+</asp:Content>
